af86e89a95d757d7b158d56b5f03073d80270ded
Ikram001 <ikramnixamani@gmail.com>
2025-05-14 12:30:18 +0500

    styling and credit?

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
-
+ <h1 class="credit">Assignment by</h1>
+ <h2 class= "credit">Ikramullah Nizamani</h2>
+ <h2 class= "credit">2k22/CSE/47</h2>
  <footer>
     <p>&copy; <?php echo date("Y"); ?> Foodie's Delight. All rights reserved.</p>
   </footer>
